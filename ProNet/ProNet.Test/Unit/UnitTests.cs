2e13e9b7861ea480f6fb0e106831ca1ae0e6cfb7
Jason Gorman <jason.gorman@codemanship.com>
2017-09-16 12:05:25 +0100

    Added dummy unit test for build

---
+++ ProNet/ProNet.Test/Unit/UnitTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ProNet.Test.Unit
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void PassingDummyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
